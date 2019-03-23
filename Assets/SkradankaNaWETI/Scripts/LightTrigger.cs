25790c05b267dc4b4d41386d35d3e302a57f60bc
Adam Zieliński <zielinski.adam@poczta.pl>
2019-03-23 18:40:55 +0100

    Add simple light detection

---
+++ Assets/SkradankaNaWETI/Scripts/LightTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightTrigger : MonoBehaviour
+{
+    [SerializeField] private VisibilityCounter _visibilityCounter;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name=="FPSController")
+            _visibilityCounter.Increment();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "FPSController")
+            _visibilityCounter.Decrement();
+    }
+}
