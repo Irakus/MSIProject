48a1a0709b21d44d662dba2c9c5ee092960216ff
Patryk Iracki <patrak122@gmail.com>
2019-05-12 18:40:50 +0200

    AI Scene

---
+++ Assets/AI/Scripts/LookForIntruders.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookForIntruders : MonoBehaviour
+{
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if(Physics.SphereCast)
+    }
+}
---
+++ Assets/AI/Scripts/PatrolStations.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PatrolStations : MonoBehaviour
+{
+    public List<Transform> stations;
+    [SerializeField]
+    private bool patrolling;
+    [SerializeField]
+    private Transform currentStation;
+    private Animator animator;
+    private NavMeshAgent navMeshAgent;
+    private void Start()
+    {
+        currentStation = stations[0];
+        navMeshAgent = gameObject.GetComponent<NavMeshAgent>();
+    }
+    private void Update()
+    {
+        if (patrolling)
+        {
+            navMeshAgent.isStopped = true;
+            SetNewDestinationIfNeeded();
+            navMeshAgent.destination = currentStation.position;
+            navMeshAgent.isStopped = false;
+        }
+    }
+
+    public void StartPatrolling()
+    {
+        patrolling = true;
+    }
+
+    public void StopPatrolling()
+    {
+        patrolling = false;
+    }
+
+    private void SetNewDestinationIfNeeded()
+    {
+        if (Vector3.Distance(transform.position, currentStation.position) < 1.0f)
+        {
+            if (stations.IndexOf(currentStation) == stations.Count - 1)
+            {
+                currentStation = stations[0];
+            }
+            else
+            {
+                currentStation = stations[stations.IndexOf(currentStation) + 1];
+            }
+        }
+    }
+}
