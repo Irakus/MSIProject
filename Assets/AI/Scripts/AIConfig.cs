e5b0946e425cd68b07bdd3d6e66919b5be2a1f90
Patryk Iracki <patrak122@gmail.com>
2019-06-02 00:15:39 +0200

    Added voicepacks system - turned off for now though

---
+++ Assets/AI/Scripts/AIConfig.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UMA.CharacterSystem;
+using UnityEngine;
+
+public class AIConfig : MonoBehaviour
+{
+    [SerializeField]
+    public VoicePack voicePack;
+
+    [SerializeField]
+    private CharacterConfig characterConfig;
+
+    enum CharacterConfig{ MaleBodyguard, FemaleJanitor }
+    void Awake()
+    {
+        DynamicCharacterAvatar avatar = GetComponent<DynamicCharacterAvatar>();
+
+        switch (characterConfig)
+        {
+            case CharacterConfig.FemaleJanitor:
+                avatar.activeRace.name = "HumanFemale";
+                voicePack = GameObject.Find("VoicePacks/FemaleJanotor").GetComponent<VoicePack>();
+                break;
+            case CharacterConfig.MaleBodyguard:
+                //avatar.activeRace.name = "HumanMale";
+                voicePack = GameObject.Find("VoicePacks/MaleBodyguard").GetComponent<VoicePack>();
+                break;
+            default:
+                Debug.Log("Please select character version first for your AI");
+                break;
+        }
+    }
+
+
+}
---
+++ Assets/AI/Scripts/VoicePack.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class VoicePack : MonoBehaviour
+{
+    [SerializeField]
+    private List<AudioSource> casualTracks;
+    [SerializeField]
+    private List<AudioSource> chasingTracks;
+    [SerializeField]
+    private List<AudioSource> spottedTracks;
+    [SerializeField]
+    private List<AudioSource> lostTracks;
+    
+    [SerializeField]
+    private AudioSource currentlyPlaying;
+
+    private static Random rnd = new Random();
+
+    public void PlaySpotted()
+    {
+        currentlyPlaying.Stop();
+        currentlyPlaying = spottedTracks[Random.Range(0, spottedTracks.Count - 1)];
+        currentlyPlaying.Play();
+    }
+
+
+    public void PlayCasual()
+    {
+        currentlyPlaying = casualTracks[Random.Range(0, casualTracks.Count - 1)];
+        currentlyPlaying.Play();
+    }
+
+    public void PlayChasing()
+    {
+        currentlyPlaying = chasingTracks[Random.Range(0, chasingTracks.Count - 1)];
+        currentlyPlaying.Play();
+    }
+
+    public void PlayLost()
+    {
+        currentlyPlaying.Stop();
+        currentlyPlaying = lostTracks[Random.Range(0, lostTracks.Count - 1)];
+        currentlyPlaying.Play();
+    }
+
+    public bool IsPlaying()
+    {
+        return currentlyPlaying.isPlaying;
+    }
+
+    void Awake()
+    {
+        currentlyPlaying = null;
+    }
+    void Update()
+    {
+        if (!currentlyPlaying.isPlaying) currentlyPlaying = null;
+    }
+}
